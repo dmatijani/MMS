3cc8b6513f8d8d9b294ef10ce85ae8d9dfd9e05a
dmatijani <dmatijani21@foi.hr>
2024-07-20 18:14:00 +0200

    Dorađeno sučelje zahtjeva za članstvom

--- Models/ViewModels/UserDataViewModel.cs
+++ Models/ViewModels/UserDataViewModel.cs
@@ -2,13 +2,15 @@
 {
 	public class UserDataViewModel
 	{
-        public UserDataViewModel(int id)
+        public UserDataViewModel(int id, string name, string value = "")
         {
             Id = id;
+            Name = name;
+            Value = value;
         }
 
         public int Id { get; set; }
-        public string Name { get; set; } = "";
-        public string Value { get; set; } = "";
+        public string Name { get; set; }
+        public string Value { get; set; }
     }
 }
