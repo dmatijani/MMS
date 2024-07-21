512726678cb1cfb0879ba2819f54fd49f7b04183
dmatijani <dmatijani21@foi.hr>
2024-07-21 19:03:29 +0200

    Profil se može pregledati, no ne i mijenjati kontakte (još)

--- Data/Repositories/UserRepository.cs
+++ Data/Repositories/UserRepository.cs
@@ -31,7 +31,7 @@ namespace MMS.Data.Repositories
 
 		public async Task<User?> Get(int id)
 		{
-			return await _context.Users.Include(u => u.Role).Where(x => x.Id == id).FirstOrDefaultAsync();
+			return await _context.Users.Include(u => u.Role).Include(u => u.UserData).Where(x => x.Id == id).FirstOrDefaultAsync();
 		}
 
 		public async Task Update(User entity)
