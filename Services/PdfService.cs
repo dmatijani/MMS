7a859c9c9e14b93963da2aa2724f81711688d6a8
dmatijani <dmatijani21@foi.hr>
2024-07-23 14:03:54 +0200

    Malo refaktoriranja

--- Services/PdfService.cs
+++ Services/PdfService.cs
@@ -22,82 +22,14 @@ namespace MMS.Services
 			_env = env;
 		}
 
-		public byte[] GenerateMembershipConfirmation(User user)
+		public byte[] GetMembershipConfirmation(User user)
 		{
 			using (MemoryStream memoryStream = new MemoryStream())
 			{
 				PdfWriter writer = new PdfWriter(memoryStream);
 				PdfDocument pdf = new PdfDocument(writer);
-				Document document = new Document(pdf);
 
-				string fontPath = Path.Combine(_env.WebRootPath, "arial.ttf");
-				var font = PdfFontFactory.CreateFont(fontPath, PdfEncodings.IDENTITY_H);
-
-				string footerText = $"Generirano {DateTime.Now.ToString("dd.MM.yyyy. u HH:mm:ss")} od strane MMS sustava";
-				pdf.AddEventHandler(PdfDocumentEvent.END_PAGE, new FooterEventHandler(document, font, footerText));
-
-				Paragraph title = new Paragraph("Potvrda o članstvu")
-					.SetTextAlignment(TextAlignment.CENTER)
-					.SetFontSize(20)
-					.SetBold()
-					.SetFont(font);
-				document.Add(title);
-
-				document.Add(new Paragraph("\n").SetFont(font));
-
-				document.Add(new Paragraph($"Zahtjev za članstvom poslan je {user.MembershipRequestDate.ToString("dd.MM.yyyy. u HH:mm:ss")}.").SetFont(font));
-				document.Add(new Paragraph($"Profil člana/ice {user.Name} {user.Surname} odobren je datuma {user.MembershipApprovalDate.ToString("dd.MM.yyyy. u HH:mm:ss")}.").SetFont(font));
-				if (user.Payments.Any())
-				{
-					Payment firstPayment = user.Payments.OrderBy(p => p.Date).First();
-					Payment lastPayment = user.Payments.OrderByDescending(p => p.Date).First();
-					document.Add(new Paragraph($"Datum prve uplate je {firstPayment.Date.ToString("dd.MM.yyyy.")}").SetFont(font));
-					document.Add(new Paragraph($"Datum zadnje uplate je {lastPayment.Date.ToString("dd.MM.yyyy.")} i članstvo vrijedi do {lastPayment.DateUntil.ToString("dd.MM.yyyy.")} (još {(lastPayment!.DateUntil - DateTime.Now.Date).Days} dana).").SetFont(font));
-				}
-
-				document.Add(new Paragraph("\n").SetFont(font));
-
-				document.Add(new Paragraph($"Ime: {user.Name}").SetFont(font));
-				document.Add(new Paragraph($"Prezime: {user.Surname}").SetFont(font));
-				document.Add(new Paragraph($"Email: {user.Email}").SetFont(font));
-
-				if (user.UserData.Any())
-				{
-					foreach (var contact in user.UserData)
-					{
-						document.Add(new Paragraph($"{contact.Name}: {contact.Value}").SetFont(font));
-					}
-				}
-
-				document.Add(new Paragraph("\n").SetFont(font));
-
-				Paragraph paymentsTitle = new Paragraph("Popis plaćanja")
-					.SetTextAlignment(TextAlignment.LEFT)
-					.SetFontSize(16)
-					.SetBold()
-					.SetFont(font);
-				document.Add(paymentsTitle);
-
-				if (user.Payments.Any())
-				{
-					Table paymentsTable = new Table(new float[] { 1, 1 });
-					paymentsTable.SetWidth(UnitValue.CreatePercentValue(100));
-					paymentsTable.AddHeaderCell("Datum plaćanja").SetFont(font);
-					paymentsTable.AddHeaderCell("Vrijedi do").SetFont(font);
-
-					foreach (var payment in user.Payments.OrderByDescending(p => p.Date))
-					{
-						paymentsTable.AddCell(new Cell().Add(new Paragraph(payment.Date.ToString("dd.MM.yyyy."))).SetFont(font));
-						paymentsTable.AddCell(new Cell().Add(new Paragraph(payment.DateUntil.ToString("dd.MM.yyyy."))).SetFont(font));
-					}
-					document.Add(paymentsTable).SetFont(font);
-				}
-				else
-				{
-					document.Add(new Paragraph("Nema zabilježenih plaćanja.").SetFont(font));
-				}
-
-				document.Close();
+				GenerateMembershipConfirmation(pdf, user);
 
 				byte[] bytes = memoryStream.ToArray();
 				return bytes;
@@ -112,6 +44,80 @@ namespace MMS.Services
 				filename += ".pdf";
 			}
 			await _js.InvokeVoidAsync("saveFile", filename, documentBase64);
+		}
+
+		private void GenerateMembershipConfirmation(PdfDocument pdf, User user)
+		{
+			Document document = new Document(pdf);
+
+			string fontPath = Path.Combine(_env.WebRootPath, "arial.ttf");
+			var font = PdfFontFactory.CreateFont(fontPath, PdfEncodings.IDENTITY_H);
+
+			string footerText = $"Generirano {DateTime.Now.ToString("dd.MM.yyyy. u HH:mm:ss")} od strane MMS sustava";
+			pdf.AddEventHandler(PdfDocumentEvent.END_PAGE, new FooterEventHandler(document, font, footerText));
+
+			Paragraph title = new Paragraph("Potvrda o članstvu")
+				.SetTextAlignment(TextAlignment.CENTER)
+				.SetFontSize(20)
+				.SetBold()
+				.SetFont(font);
+			document.Add(title);
+
+			document.Add(new Paragraph("\n").SetFont(font));
+
+			document.Add(new Paragraph($"Zahtjev za članstvom poslan je {user.MembershipRequestDate.ToString("dd.MM.yyyy. u HH:mm:ss")}.").SetFont(font));
+			document.Add(new Paragraph($"Profil člana/ice {user.Name} {user.Surname} odobren je datuma {user.MembershipApprovalDate.ToString("dd.MM.yyyy. u HH:mm:ss")}.").SetFont(font));
+			if (user.Payments.Any())
+			{
+				Payment firstPayment = user.Payments.OrderBy(p => p.Date).First();
+				Payment lastPayment = user.Payments.OrderByDescending(p => p.Date).First();
+				document.Add(new Paragraph($"Datum prve uplate je {firstPayment.Date.ToString("dd.MM.yyyy.")}").SetFont(font));
+				document.Add(new Paragraph($"Datum zadnje uplate je {lastPayment.Date.ToString("dd.MM.yyyy.")} i članstvo vrijedi do {lastPayment.DateUntil.ToString("dd.MM.yyyy.")} (još {(lastPayment!.DateUntil - DateTime.Now.Date).Days} dana).").SetFont(font));
+			}
+
+			document.Add(new Paragraph("\n").SetFont(font));
+
+			document.Add(new Paragraph($"Ime: {user.Name}").SetFont(font));
+			document.Add(new Paragraph($"Prezime: {user.Surname}").SetFont(font));
+			document.Add(new Paragraph($"Email: {user.Email}").SetFont(font));
+
+			if (user.UserData.Any())
+			{
+				foreach (var contact in user.UserData)
+				{
+					document.Add(new Paragraph($"{contact.Name}: {contact.Value}").SetFont(font));
+				}
+			}
+
+			document.Add(new Paragraph("\n").SetFont(font));
+
+			Paragraph paymentsTitle = new Paragraph("Popis plaćanja")
+				.SetTextAlignment(TextAlignment.LEFT)
+				.SetFontSize(16)
+				.SetBold()
+				.SetFont(font);
+			document.Add(paymentsTitle);
+
+			if (user.Payments.Any())
+			{
+				Table paymentsTable = new Table(new float[] { 1, 1 });
+				paymentsTable.SetWidth(UnitValue.CreatePercentValue(100));
+				paymentsTable.AddHeaderCell("Datum plaćanja").SetFont(font);
+				paymentsTable.AddHeaderCell("Vrijedi do").SetFont(font);
+
+				foreach (var payment in user.Payments.OrderByDescending(p => p.Date))
+				{
+					paymentsTable.AddCell(new Cell().Add(new Paragraph(payment.Date.ToString("dd.MM.yyyy."))).SetFont(font));
+					paymentsTable.AddCell(new Cell().Add(new Paragraph(payment.DateUntil.ToString("dd.MM.yyyy."))).SetFont(font));
+				}
+				document.Add(paymentsTable).SetFont(font);
+			}
+			else
+			{
+				document.Add(new Paragraph("Nema zabilježenih plaćanja.").SetFont(font));
+			}
+
+			document.Close();
 		}
 	}
 }
